49314627c0fc63e1bfcf2ea6007663e1ebe7fb56
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-02-06 19:31:35 -0430

    Eliminado el código de ejecución del módulo como tal

--- pywrapper-config/pywrapper_config.py
+++ pywrapper-config/pywrapper_config.py
@@ -64,20 +64,3 @@ class Config(object):
         self._config.write(open(self._cnffile,'w'))
 
 
-
-if __name__ == '__main__':
-    config = Config("./conf/tests.conf")
-    #Show sections 
-    print("Sections: {0} ".format(config.show_sections()))
-    #Show server section
-    print("Items server section: {0} ".format(config.show_item_section("server")))
-    #Show ip server
-    print("IP: {0}".format(config.show_value_item("server","ip")))
-    #Show config file
-    print("Config file: {0}".format(config.cnffile))
-    #Change config file
-    config.cnffile = "./conf/tests2.py"
-    print("Config file: {0}".format(config.cnffile))
-    #Show sections from new config file 
-    print("Secciones: {0}".format(config.show_sections()))
-
